4631f46e0dad3cb8f3182b27551384d5e313648e
Beatrice <beacitterio@gmail.com>
2025-01-04 19:04:39 +0100

    removed comments

--- imdb_api.py
+++ imdb_api.py
@@ -5,7 +5,6 @@ BASE_URL = 'https://api.themoviedb.org/3'
 
 
 def fetch_movie_info(query):
-    # Search for the movie
     search_url = f"{BASE_URL}/search/movie?api_key={API_KEY}&query={query}"
     search_response = requests.get(search_url).json()
 
@@ -26,24 +25,19 @@ def fetch_movie_info(query):
     title = search_response['results'][0]['title']
     plot = search_response['results'][0]['overview']
 
-    # Get movie details
     details_url = f"{BASE_URL}/movie/{movie_id}?api_key={API_KEY}"
     details_response = requests.get(details_url).json()
 
     length = details_response['runtime']
 
-    # Extract genres
     genres = [genre['name'] for genre in details_response['genres']]
 
-    # Extract poster URL
     poster_path = details_response.get('poster_path')
     poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}" if poster_path else 'No poster available'
 
-    # Get credits (actors)
     credits_url = f"{BASE_URL}/movie/{movie_id}/credits?api_key={API_KEY}"
     credits_response = requests.get(credits_url).json()
 
-    # Extract top 5 actors
     actors = [cast['name'] for cast in credits_response['cast'][:5]]
 
     director = next((crew['name'] for crew in credits_response['crew'] if crew['job'] == 'Director'), 'Not Available')
@@ -63,13 +57,10 @@ def fetch_movie_info(query):
         'Platforms': platforms
     }
 
-
 def fetch_trailer(movie_id):
-    # Fetch video details for the movie
     url = f"{BASE_URL}/movie/{movie_id}/videos?api_key={API_KEY}"
     response = requests.get(url).json()
 
-    # Find the trailer in the video list
     for video in response['results']:
         if video['type'] == 'Trailer' and video['site'] == 'YouTube':
             return f"https://www.youtube.com/watch?v={video['key']}"  # Trailer URL
@@ -78,7 +69,6 @@ def fetch_trailer(movie_id):
 
 
 def fetch_platforms(movie_id):
-    # Fetch watch providers
     url = f"{BASE_URL}/movie/{movie_id}/watch/providers?api_key={API_KEY}"
     response = requests.get(url).json()
 
