81acdf0aed57892584fba37bbcae1e26fc7ddd49
Beatrice <beacitterio@gmail.com>
2025-01-07 11:07:25 +0100

    change port

--- app_render.py
+++ app_render.py
@@ -74,5 +74,5 @@ def get_recommendations():
     return jsonify(recommendations)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 5001))  
     app.run(host="0.0.0.0", port=port)
